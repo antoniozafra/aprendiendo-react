f584729ae115cd4c501f15dac1beddf374c44c52
Antonio Zafra <antoniozafra1997@gmail.com>
2023-07-31 15:55:40 +0200

    52. Mi primera Prueba y configuraciones iniciales

---
+++ 03-counter-app-vite/test/demo.test.js
@@ -0,0 +1,12 @@
+describe('Pruebas en demo test', () => {
+
+   test("Esta prueba no debe de fallar", () => {
+
+        if(0 === 1){
+            throw new Error("No se puede dividir en 0");
+        }
+
+   });
+    
+  
+})
