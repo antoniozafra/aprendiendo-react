538c510845937c89c3622c25a6ec66a08dc5572f
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-04 17:20:49 +0200

    103. Pruebas en el componentes -  GifGridItem

--- gif-expert-app/test/components/GifItem.test.js
+++ gif-expert-app/test/components/GifItem.test.js
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem";
 describe('Pruebas en el componentes GifItem', () => {
     const title = "Titulo de ejemplo";
-    const url = "https://www.example.com";
+    const url = "https://www.example.com/";
 
     test('Debe de hacer match con el snapshot', () => {
 
@@ -10,6 +10,25 @@ describe('Pruebas en el componentes GifItem', () => {
       
         expect(container).toMatchSnapshot();
     });
+
+    test('Debe de mostrar la img con url y alt indicado', () => {
+
+       render(<GifItem title={title} url={url}/>);
+
+        expect(screen.getByRole('img').alt).toBe(title) ;
+        expect(screen.getByRole('img').src).toBe(url) ;
+
+    });
+    
+  
+    test('Debe de mostrar el titulo en el componente', () => {
+
+        render(<GifItem title={title} url={url}/>);
+
+        expect(screen.getByText(title)).toBeTruthy();
+
+      
+    })
     
   
 })
