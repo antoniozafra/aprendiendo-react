f0704998bbbf2edb150c1442a39f3c0a66504d72
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-05 12:04:28 +0200

    115. Inicio del Proyecto - HooksApp

---
+++ 04-gif-expert-app/test/GifExpertApp.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import GifExpertApp from "../src/GifExpertApp";
+
+describe('Pruebas en el componente <GifExpertApp/>', () => {
+  
+
+    test('Debe de ', () => {
+      render(<GifExpertApp/>);
+
+      screen.debug();
+    });
+    
+});
