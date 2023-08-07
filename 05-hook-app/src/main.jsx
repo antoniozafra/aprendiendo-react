ee65109b1758084ea7e42d6005ec98cd78225de6
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-07 12:43:01 +0200

    120. UseLayoutEffect

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -6,6 +6,8 @@ import SimpleForm from './02-useEffect/SimpleForm'
 import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
 import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
+
 
 
 
@@ -17,6 +19,6 @@ import { FocusScreen } from './04-useRef/FocusScreen'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   // <React.StrictMode>
-    <FocusScreen />
+    <Layout />
   // </React.StrictMode>,
 )
