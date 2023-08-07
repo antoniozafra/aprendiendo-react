5d96e52283c039f4207e3f9e2b5dedf7a765d95c
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-07 13:06:06 +0200

    131.- Memo - Método de React

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -7,6 +7,7 @@ import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
 import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import Memorize from './06-memos/Memorize'
 
 
 
@@ -19,6 +20,6 @@ import { Layout } from './05-useLayoutEffect/Layout'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   // <React.StrictMode>
-    <Layout />
+    <Memorize />
   // </React.StrictMode>,
 )
