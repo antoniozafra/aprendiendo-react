e4319f169ad9ae13023a6ea07ef6a6880c015fe7
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-07 12:28:19 +0200

    129. UseRef Primer Uso

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -5,6 +5,8 @@ import {CounterWithCustomHook} from './01-useState/CounterWithCustomHook'
 import SimpleForm from './02-useEffect/SimpleForm'
 import FormWithCustomHook from './02-useEffect/FormWithCustomHook'
 import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
+
 
 
 // import HooksApp from './HooksApp'
@@ -15,6 +17,6 @@ import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   // <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   // </React.StrictMode>,
 )
