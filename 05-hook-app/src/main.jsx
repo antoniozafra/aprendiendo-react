c25a3540f422404293687f2cae592c7b8cd5b107
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-07 13:17:46 +0200

    132. useMemo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -8,6 +8,8 @@ import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
 import Memorize from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
+
 
 
 
@@ -20,6 +22,6 @@ import Memorize from './06-memos/Memorize'
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   // <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   // </React.StrictMode>,
 )
