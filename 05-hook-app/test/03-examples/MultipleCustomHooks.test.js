426881db7f3630a7b9d3e248614a58f2310b464f
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-08 16:25:51 +0200

    171. Evaluar respuesta del useFetch

--- 05-hook-app/test/03-examples/MultipleCustomHooks.test.js
+++ 05-hook-app/test/03-examples/MultipleCustomHooks.test.js
@@ -1,21 +1,57 @@
-import { getByRole, render, screen } from "@testing-library/react";
+import { fireEvent, getByRole, render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../src/03-examples/MultipleCustomHooks";
+import { useFetch } from "../../src/hooks";
+
+jest.mock('../../src/hooks');
+
 
 describe('Pruebas en MultipleCustomHook', () => {
-  
+
 
     test('Debe de mostrar el componente por defecto', () => {
-    
-        render(<MultipleCustomHooks/>);
+
+        useFetch.mockReturnValue({ data: null, isLoading: true, hasError: null });
+
+        render(<MultipleCustomHooks />);
 
         expect(screen.getByText('Cargando...'));
         expect(screen.getByText('Breaking Bad Quotes'));
 
-        const nextButton = screen.getByRole('button',{name:"Next Quote"});
+        const nextButton = screen.getByRole('button', { name: "Next Quote" });
         expect(nextButton.disabled).toBeTruthy();
 
-        screen.debug();
-    
+
     });
-    
+
+    test('Debe de mostrar un Quote', () => {
+
+        useFetch.mockReturnValue({
+            data: [{ author: 'Antonio', quote: 'Hola soy Antonio' }],
+            isLoading: true,
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+
+
+    });
+
+    test('Debe de llamar la funcion de incrmentar', () => {
+
+        useFetch.mockReturnValue({
+            data: [{ author: 'Antonio', quote: 'Hola soy Antonio' }],
+            isLoading: true,
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', { name: "Next Quote" });
+        fireEvent.click(nextButton);
+
+        screen.debug();
+
+    });
+
+
+
 });
