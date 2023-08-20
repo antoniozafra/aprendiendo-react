7bb06dab692e7fd1eece98892e688b11eae8cfbd
Antonio Zafra <antoniozafra1997@gmail.com>
2023-08-20 19:31:55 +0200

    Fin Seccion 22

--- 10-calendar/src/calendar/components/Navbar.jsx
+++ 10-calendar/src/calendar/components/Navbar.jsx
@@ -1,16 +1,18 @@
-export const Navbar = () => {
-    return (
-        <div className="navbar navbar-dark bg-dark mb-4 px-4">
-            <span className="navbar-brand">
-                <i className="fas fa-calendar-alt"></i>
-                &nbsp;
-                Antonio
-            </span>
 
-            <button className="btn btn-outline-danger">
-                <i className="fa fa-sign-out-alt"></i>
-                Salir
-            </button>
-        </div>
-    )
+
+export const Navbar = () => {
+  return (
+    <div className="navbar navbar-dark bg-dark mb-4 px-4">
+        <span className="navbar-brand">
+            <i className="fas fa-calendar-alt"></i>
+            &nbsp;
+            Fernando
+        </span>
+
+        <button className="btn btn-outline-danger">
+            <i className="fas fa-sign-out-alt"></i>
+            <span>Salir</span>
+        </button>
+    </div>
+  )
 }
--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,49 +1,80 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
+import { useState } from 'react';
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-import { Navbar } from "../components/Navbar"
-import { addHours } from 'date-fns'
+import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete} from '../';
 
-const locales = {
-  'en-US': enUS,
-  
-};
+import { localizer, getMessagesES } from '../../helpers';
+import { useUiStore, useCalendarStore } from '../../hooks';
 
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
 
-const events = [{
-  title:  'Cumpleaños del jefe',
-  notes: 'Comprar pastel',
-  start: new Date(),
-  end: addHours(new Date(), 2),
-  bgColor: '#fafafa',
-
-}]
 
 export const CalendarPage = () => {
 
+  const { openDateModal } = useUiStore();
+  const { events, setActiveEvent } = useCalendarStore();
+
+  const [ lastView, setLastView ] = useState(localStorage.getItem('lastView') || 'week' );
+
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+
+    const style = {
+      backgroundColor: '#347CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
+
+  const onDoubleClick = ( event ) => {
+    // console.log({ doubleClick: event });
+    openDateModal();
+  }
+
+  const onSelect = ( event ) => {
+    // console.log({ click: event });
+    setActiveEvent( event );
+  }
+
+  const onViewChanged = ( event ) => {
+    localStorage.setItem('lastView', event );
+    setLastView( event )
+  }
+
+
+
   return (
     <>
-      <Navbar/>
+      <Navbar />
 
       <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 'calc(100vh - 80px)' }}
-    />
+        culture='es'
+        localizer={ localizer }
+        events={ events }
+        defaultView={ lastView }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
+        onDoubleClickEvent={ onDoubleClick }
+        onSelectEvent={ onSelect }
+        onView={ onViewChanged }
+      />
+
+
+      <CalendarModal />
+      
+      <FabAddNew />
+      <FabDelete />
+
 
     </>
   )
